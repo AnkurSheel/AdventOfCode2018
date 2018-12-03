421cafaa822162eea16de7683721da536124216d
Ankur Sheel <ankur.sheel@gmail.com>
2018-12-04 12:05:51 +1300

    add Attribute to read testData from file

---
+++ AdventOfCode2018.Tests/JsonFileDataAttribute.cs
@@ -0,0 +1,77 @@
+﻿// https://andrewlock.net/creating-a-custom-xunit-theory-test-dataattribute-to-load-data-from-json-files/
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit.Sdk;
+
+namespace AdventOfCode2018.Tests
+{
+	class TestObject
+	{
+		public List<int> Data { get; set; }
+		public int Result { get; set; }
+	}
+
+	public class JsonFileDataAttribute : DataAttribute
+	{
+		private readonly string _filePath;
+		private readonly string _propertyName;
+
+		public JsonFileDataAttribute(string filePath)
+		{
+			_filePath = filePath;
+		}
+
+		public JsonFileDataAttribute(string filePath, string propertyName)
+		{
+			_filePath = filePath;
+			_propertyName = propertyName;
+		}
+
+		public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+		{
+			if (testMethod == null) { throw new ArgumentNullException(nameof(testMethod)); }
+
+			var parameters = testMethod.GetParameters();
+			// Get the absolute path to the JSON file
+			var path = Path.IsPathRooted(_filePath)
+				? _filePath
+				: Path.GetRelativePath(Directory.GetCurrentDirectory(), _filePath);
+
+			if (!File.Exists(path))
+			{
+				throw new ArgumentException($"Could not find file at path: {path}");
+			}
+
+			// Load the file
+			var fileData = File.ReadAllText(_filePath);
+
+			if (string.IsNullOrEmpty(_propertyName))
+			{
+				//whole file is the data
+				return GetData(fileData);
+			}
+
+			// Only use the specified property as the data
+			var allData = JObject.Parse(fileData);
+			var data = allData[_propertyName].ToString();
+			return GetData(data);
+		}
+
+		private IEnumerable<object[]> GetData(string jsonData)
+		{
+			var datalist = JsonConvert.DeserializeObject<List<TestObject>>(jsonData);
+			var objectList = new List<object[]>();
+			foreach (var data in datalist)
+			{
+				objectList.Add(new object[] {data.Data, data.Result});
+			}
+
+			return objectList;
+		}
+	}
+}
