158780457c4ba49a8bd127cb743fc31c7cc30c98
Ankur Sheel <ankur.sheel@gmail.com>
2018-12-04 13:51:25 +1300

    make JsonFileDataAtrribute be generic

---
+++ AdventOfCode2018.Tests/TestObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2018.Tests
+{
+	class TestObject<T1, T2>
+	{
+		public List<T1> Data { get; set; }
+	
+		public T2 Result { get; set; }
+	}
+}
