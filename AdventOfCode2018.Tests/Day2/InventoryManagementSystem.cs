d369ef8e01350743f3234f6d7ae4755e0e36be51
Ankur Sheel <ankur.sheel@gmail.com>
2018-12-04 14:58:33 +1300

    Part 1

---
+++ AdventOfCode2018.Tests/Day2/InventoryManagementSystem.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace AdventOfCode2018.Tests.Day2
+{
+	public class InventoryManagementSystem
+	{
+		public int CalculateChecksum(List<string> boxIds)
+		{
+			int twiceAppearingLetterCount = 0;
+			int thriceAppearingLetterCount = 0;
+
+			foreach (var id in boxIds)
+			{
+				var seenLetters = new Dictionary<char, int>();
+				foreach (var letter in id)
+				{
+					if (seenLetters.ContainsKey(letter))
+					{
+						seenLetters[letter] += 1;
+					}
+					else
+					{
+						seenLetters[letter] = 1;
+					}
+				}
+
+				bool twiceAppearingLetterFound = false;
+				bool thriceAppearingLetterFound = false;
+				foreach (var letterCounts in seenLetters)
+				{
+					if (!twiceAppearingLetterFound && letterCounts.Value == 2)
+					{
+						twiceAppearingLetterCount++;
+						twiceAppearingLetterFound = true;
+					}
+					if (!thriceAppearingLetterFound && letterCounts.Value == 3)
+					{
+						thriceAppearingLetterCount++;
+						thriceAppearingLetterFound = true;
+					}
+				}
+			}
+
+			return twiceAppearingLetterCount * thriceAppearingLetterCount;
+		}
+	}
+}
---
+++ AdventOfCode2018.Tests/Day2/InventoryManagementSystemTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace AdventOfCode2018.Tests.Day2
+{
+	public class InventoryManagementSystemTest
+	{
+		private readonly InventoryManagementSystem _inventoryManagementSystem;
+
+		public InventoryManagementSystemTest()
+		{
+			_inventoryManagementSystem = new InventoryManagementSystem();
+		}
+
+		[Theory]
+		[JsonFileData("Day2/testData.json", "Part1", typeof(string), typeof(int))]
+		public void CalculateChecksumReturnsCorrectChecksum(List<string> data, int expectedResult)
+		{
+			var result = _inventoryManagementSystem.CalculateChecksum(data);
+			Assert.Equal(expectedResult, result);
+		}
+	}
+}
