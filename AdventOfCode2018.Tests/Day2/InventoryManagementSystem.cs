3510e3ee052b8e9ddd2593751646504eeb4815e9
Ankur Sheel <ankur.sheel@gmail.com>
2018-12-04 15:56:49 +1300

    Part 2

--- AdventOfCode2018.Tests/Day2/InventoryManagementSystem.cs
+++ AdventOfCode2018.Tests/Day2/InventoryManagementSystem.cs
@@ -1,9 +1,9 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Text;
 
 namespace AdventOfCode2018.Tests.Day2
 {
-	public class InventoryManagementSystem
+    public class InventoryManagementSystem
 	{
 		public int CalculateChecksum(List<string> boxIds)
 		{
@@ -44,5 +44,44 @@ namespace AdventOfCode2018.Tests.Day2
 
 			return twiceAppearingLetterCount * thriceAppearingLetterCount;
 		}
+
+		public string CalculateCommonIdDifferingByOneChar(List<string> boxIds)
+		{
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < boxIds.Count; i++)
+			{
+				var id1 = boxIds[i];
+				for (int j = i+1; j < boxIds.Count; j++)
+				{
+					sb.Clear();
+					var id2 = boxIds[j];
+					int charDifferencesFound = 0;
+					for (int k = 0, l = 0; k < id1.Length && l < id2.Length; k++, l++)
+					{
+						if (id1[k] == id2[l])
+						{
+							sb.Append(id1[k]);
+						}
+						else
+						{
+							charDifferencesFound++;
+							if (charDifferencesFound > 1)
+							{
+								break;
+							}
+						}
+					}
+
+					if (charDifferencesFound == 1)
+					{
+						return sb.ToString();
+					}
+				}
+			}
+
+			return string.Empty;
+
+		}
+		
 	}
 }
--- AdventOfCode2018.Tests/Day2/InventoryManagementSystemTest.cs
+++ AdventOfCode2018.Tests/Day2/InventoryManagementSystemTest.cs
@@ -14,9 +14,17 @@ namespace AdventOfCode2018.Tests.Day2
 
 		[Theory]
 		[JsonFileData("Day2/testData.json", "Part1", typeof(string), typeof(int))]
-		public void CalculateChecksumReturnsCorrectChecksum(List<string> data, int expectedResult)
+		public void CalculateChecksumReturnsCorrectChecksum(List<string> boxIds, int expectedResult)
 		{
-			var result = _inventoryManagementSystem.CalculateChecksum(data);
+			var result = _inventoryManagementSystem.CalculateChecksum(boxIds);
+			Assert.Equal(expectedResult, result);
+		}
+
+		[Theory]
+		[JsonFileData("Day2/testData.json", "Part2", typeof(string), typeof(string))]
+		public void CalculateCommonIdDifferingByOneCharReturnsCorrectString(List<string> boxIds, string expectedResult)
+		{
+			var result = _inventoryManagementSystem.CalculateCommonIdDifferingByOneChar(boxIds);
 			Assert.Equal(expectedResult, result);
 		}
 	}
