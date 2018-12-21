024fbafce4ba6fac219959bd0f8e2baa67e49304
Ankur Sheel <ankur.sheel@gmail.com>
2018-12-22 00:09:04 +1300

    Part 1

---
+++ AdventOfCode2018.Tests/Day4/Record.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode2018.Tests.Day4
+{
+    public class Record
+    {
+        public Record()
+        {
+            SleepMinutes = new Dictionary<int, int>();
+        }
+
+        public Record(Record recordValue)
+        {
+            GuardId = recordValue.GuardId;
+            TotalSleepTime = recordValue.TotalSleepTime;
+            SleepMinutes = recordValue.SleepMinutes;
+            SleepTime = recordValue.SleepTime;
+            WakeTime = recordValue.WakeTime;
+        }
+
+
+        public int GuardId { get; set; }
+
+        public int TotalSleepTime { get; set; }
+
+        public Dictionary<int, int> SleepMinutes { get; }
+
+        public int SleepTime { get; set; }
+
+        public int WakeTime { get; set; }
+    }
+}
---
+++ AdventOfCode2018.Tests/Day4/ReposeRecordTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+using Xunit;
+
+namespace AdventOfCode2018.Tests.Day4
+{
+    public class ReposeRecordTests
+    {
+        private readonly ReposeRecord _reposeRecord;
+
+        public ReposeRecordTests()
+        {
+            _reposeRecord = new ReposeRecord();
+        }
+
+        [Theory]
+        [JsonFileData("Day4/testData.json", "Part1", typeof(string), typeof(int))]
+        public void GetMostMinutesAsleepGuardIdMultipliedByTheMinuteTheGuardSpendsAsleepMostReturnsCorrectResult(List<string> records, int expectedResult)
+        {
+            var result = _reposeRecord.GetMostMinutesAsleepGuardIdMultipliedByTheMinuteTheGuardSpendsAsleepMost(records);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
