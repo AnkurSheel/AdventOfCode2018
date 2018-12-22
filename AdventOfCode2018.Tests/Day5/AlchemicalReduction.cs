444d0ce3344d07c02b01d0672f6cc40e2d32fcb2
Ankur Sheel <ankur.sheel@gmail.com>
2018-12-23 00:09:51 +1300

    Part 1

---
+++ AdventOfCode2018.Tests/Day5/AlchemicalReduction.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2018.Tests.Day5
+{
+    public class AlchemicalReduction
+    {
+        private const int DifferenceBetweenLowerAndUpperCase = 32;
+
+        public int GetRemainingUnits(string polymer)
+        {
+            var stack = new Stack<char>();
+            foreach (var unit in polymer)
+            {
+                if (stack.Count == 0)
+                {
+                    stack.Push(unit);
+                }
+                else
+                {
+                    var lastChar = stack.Peek();
+                    if (Math.Abs(unit - lastChar) == DifferenceBetweenLowerAndUpperCase)
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        stack.Push(unit);
+                    }
+                }
+            }
+
+            return stack.Count;
+        }
+    }
+}
---
+++ AdventOfCode2018.Tests/Day5/AlchemicalReductionTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace AdventOfCode2018.Tests.Day5
+{
+    public class AlchemicalReductionTests
+    {
+        private readonly AlchemicalReduction _alchemicalReduction;
+
+        public AlchemicalReductionTests()
+        {
+            _alchemicalReduction = new AlchemicalReduction();
+        }
+
+        [Theory]
+        [JsonFileData("Day5/testData.json", "Part1", typeof(string), typeof(int))]
+        public void GetRemainingUnitsReturnsCorrectNumberOfUnits(string polymer, int expectedResult)
+        {
+            var result = _alchemicalReduction.GetRemainingUnits(polymer);
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
